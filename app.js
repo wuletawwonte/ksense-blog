389e8bda1c2649703281ed7f54f3ecf9291b555c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-24 11:47:41 +0300

    Load users from api and show in a table

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 window.addEventListener("load", () => {
 
+    const BASE_URL = 'https://jsonplaceholder.typicode.com/';
     const app = document.getElementById("app");
+    const usersTable = document.createElement("table");
 
+    fetch(BASE_URL + "users")
+        .then(response => response.json())
+        .then(data => {
+            usersTable.innerHTML = `
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Actions
+                    </th>
+                </tr>
+                ${
+                    data.map(user => {
+                        return `
+                            <tr>
+                                <td>${user.name}</td>
+                                <td><button>Posts</button></td>
+                            </tr>
+                        `;
+                    }).join('')   
+                }
+            `;
+            app.appendChild(usersTable);
+        })
 
 });
