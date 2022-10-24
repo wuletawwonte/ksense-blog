971ce9a28c16d0af3b2e44946b3829c763ba6d0d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-24 12:53:19 +0300

    Fix linter errors

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
+  const BASE_URL = 'https://jsonplaceholder.typicode.com/';
+  const app = document.getElementById('app');
 
-    const BASE_URL = 'https://jsonplaceholder.typicode.com/';
-    const app = document.getElementById("app");
+  function loadUsers(url, parentNode) {
+    const usersTable = document.createElement('table');
 
-    loadUsers(BASE_URL + "users", app);
-
-    function loadUsers(url, parentNode) {
-        const usersTable = document.createElement("table");
-
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            usersTable.innerHTML = `
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        usersTable.innerHTML = `
                 <tr>
                     <th>
                         Name
@@ -21,48 +18,45 @@ window.addEventListener("load", () => {
                     </th>
                 </tr>
                 ${
-                    data.map(user => {
-                        return `
+  data.map((user) => `
                             <tr>
                                 <td>${user.name}</td>
                                 <td class="action"><button class="btn" data-id=${user.id}>Posts</button></td>
                             </tr>
-                        `;
-                    }).join('')   
-                }
+                        `).join('')
+}
             `;
-            parentNode.appendChild(usersTable);
+        parentNode.appendChild(usersTable);
 
-            const btns = document.querySelectorAll('.btn');
+        const btns = document.querySelectorAll('.btn');
 
-            btns.forEach(btn => {
-                btn.addEventListener('click', e => {
-                    const userId = e.target.dataset.id;
-                    fetch(`${url}/${userId}/posts`)
-                        .then(response => response.json())
-                        .then(posts => {
-                            const postsList = `
+        btns.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            const userId = e.target.dataset.id;
+            fetch(`${url}/${userId}/posts`)
+              .then((response) => response.json())
+              .then((posts) => {
+                const postsList = `
                             <h2>Posts</h2>
                             <ul class="posts">
                                 ${
-                                    posts.map(post => {
-                                        return `
+  posts.map((post) => `
                                         <li>
                                             <details>
                                                 <summary>${post.title}</summary>
                                                 <p>${post.body}</p>
                                             </details>  
                                         </li>  
-                                        `
-                                    }).join('')
-                                }
+                                        `).join('')
+}
                             </ul>
                             `;
-                            parentNode.innerHTML = postsList;
-                        });
-                })
-            })
+                parentNode.innerHTML = postsList;
+              });
+          });
         });
-    }
+      });
+  }
 
+  loadUsers(`${BASE_URL}users`, app);
 });
